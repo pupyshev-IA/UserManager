b37b22e7bed1e8e3415db234eae1b6e971fbace3
Pupyshev Ilya <PupyshevIA@abdgtl.com>
2024-09-09 14:02:28 +0300

    Added User entity

---
+++ Abdt.Loyal.UserManager.Domain/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Abdt.Loyal.UserManager.Domain
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public DateTimeOffset RegistredAt { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
